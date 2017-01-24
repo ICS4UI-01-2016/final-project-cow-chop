f7cb49e776d250690a5c7e55507f5e54bc21745f
kulla6503 <kulla6503@HRH-IC0027340.wrdsb.ca>
2017-01-24 14:18:36 -0500

    Starting Menu Almost Done

--- core/src/States/MenuState.java
+++ core/src/States/MenuState.java
@@ -4,63 +4,96 @@
  */
 package States;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
+import com.mygdx.game.MyGdxGame;
 
 /**
  *
  * @author kulla6503
  */
-public class MenuState extends State{
+public class MenuState extends State {
+
     private Texture bg;
     //
-    private Texture button;
-    private Rectangle playButton;
+    private Texture ImageOfplayButton;
+    private Rectangle playbutton;
     // How To Play option instance variables
-    private Texture HowToPlayButton;
+    private Texture ImageOfHowToPlayButton;
     private Rectangle HowToPlayButton;
-  
-    
-    public MenuState(Statemanager gsm){
-        super(gsm); bg = new Texture("bg.png");
-        playButton = new Texture("playButton.png");
-        setCameraView(FlappyBird.WIDTH, FlappyBird.HEIGHT);
-        setCameraPosition(getViewWidth()/2, getViewHeight()/2);    }  
-    
-    
+
+    public MenuState(Statemanager gsm) {
+
+
+        super(gsm);
+        //
+        bg = new Texture("");
+        //
+        ImageOfplayButton = new Texture("playButton.png");
+        // 
+        playbutton = new Rectangle(419, 409, 176, 50);
+        //
+        ImageOfHowToPlayButton = new Texture("playButton.png");
+        //
+        HowToPlayButton = new Rectangle(110, 409, 175, 50);
+        // 
+
+        setCameraView(MyGdxGame.WIDTH, MyGdxGame.HEIGHT);
+    }
+
     @Override
-    public void render(SpriteBatch batch) {  
+    public void render(SpriteBatch batch) {
         // Sets camera properly
         batch.setProjectionMatrix(getCombinedCamera());
         // Beginning the drawings
         batch.begin();
         // Drawing the rectangle behind the Play option
-        batch.draw(picOfPlayButton, playButton.x, playButton.y, playButton.width, playButton.height);
+        batch.draw(ImageOfplayButton, playbutton.x, playbutton.y, playbutton.width, playbutton.height);
         // Drawing the rectangle behind the How To Play option
-        batch.draw(picOfHowToPlayButton, howToPlayButton.x, howToPlayButton.y, howToPlayButton.width, howToPlayButton.height);
-        // Drawing the rectangle behind the description option
-        batch.draw(picOfDescriptionButton, descriptionButton.x, descriptionButton.y, descriptionButton.width, descriptionButton.height);
+        batch.draw(ImageOfHowToPlayButton, HowToPlayButton.x, HowToPlayButton.y, HowToPlayButton.width, HowToPlayButton.height);
         // Drawing the background image
         batch.draw(bg, 0, 0, getViewWidth(), getViewHeight());
         batch.end();
-      
+
     }
 
     @Override
     public void update(float deltaTime) {
-        
     }
 
     @Override
     public void handleInput() {
-        
+        // Get the mouse click/touch position of the user
+        Vector3 touch = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
+
+        // 
+        if (touch.x >= HowToPlayButton.x && touch.x <= HowToPlayButton.x + HowToPlayButton.width
+                && touch.y >= HowToPlayButton.y && touch.y <= HowToPlayButton.y + HowToPlayButton.height) {
+            // 
+            Statemanager gsm = getStateManager();
+
+
+
+        }
+
+        // If the "Play" button is clicked on, change the screen to Choose Player Amount screen
+        if (touch.x >= playbutton.x && touch.x <= playbutton.x + playbutton.width
+                && touch.y >= playbutton.y && touch.y <= playbutton.y + playbutton.height) {
+            // Call the state manager in order to change states!
+            Statemanager gsm = getStateManager();
+
+        }
     }
 
     @Override
     public void dispose() {
-        
+        // Dispose all the images used within this state
+        bg.dispose();
+
+        ImageOfHowToPlayButton.dispose();
+        ImageOfplayButton.dispose();
     }
-    
-    
 }
