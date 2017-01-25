1f98eae7c3dd8afed9e575772bc7df856b014642
kulla6503 <kulla6503@HRH-IC0027340.wrdsb.ca>
2017-01-25 12:12:01 -0500

    I finished the states, tanks and bullet

--- core/src/States/State.java
+++ core/src/States/State.java
@@ -20,47 +20,85 @@ public abstract class State {
         stateManager = sm;
         cam = new OrthographicCamera();
     }
-    
+    // Create the abstract method for a state
     public abstract void render(SpriteBatch batch);
+    // Create the abstract method for a state
     public abstract void update(float deltaTime);
+    // Create the abstract method for a state
     public abstract void handleInput();
+    // Create the abstract method for a state
     public abstract void dispose();
-    
+    /**
+     * State manager is returned 
+     * @return state manager
+     */
     public Statemanager getStateManager(){
         return stateManager;
     }
-    
+    /**
+     * orthographic cameraReturn is returned 
+     * @return the camera
+     */
     public OrthographicCamera getCamera(){
         return cam;
     }
-    
+    /**
+     * Camera's current view is set 
+     *
+     * @param width the width of the camera
+     * @param height the heigh of the camera
+     */
     public void setCameraView(float width, float height){
         cam.setToOrtho(false, width, height);
         cam.update();
     }
-    
+     /**
+     * Camera's current position is set 
+     *
+     * @param x the x value of the camera
+     * @param y the y value of the camera
+     */
     public void setCameraPosition(float x, float y){
         cam.position.x = x;
         cam.position.y = y;
         cam.update();
     }
-    
+    /**
+     * Return the combined camera
+     * @return the combined camera
+     */
      public Matrix4 getCombinedCamera(){
         return cam.combined;
     }
-    
+    /**
+     * Get the camera's view width
+     *
+     * @return the camera's view width
+     */
     public float getViewWidth(){
         return cam.viewportWidth;
     }
     
+    /**
+     * Get the camera's view height
+     * @return the camera's view height
+     */
     public float getViewHeight(){
         return cam.viewportHeight;
     }
     
+    /**
+     * Get the camera's x position
+     * @return the camera's x position
+     */
     public float getCameraX(){
         return cam.position.x;
     }
     
+    /**
+     * Get the camera's y position 
+     * @return the camera's y position
+     */
     public float getCameraY(){
         return cam.position.y;
     }
--- core/src/States/Statemanager.java
+++ core/src/States/Statemanager.java
@@ -12,36 +12,62 @@ import java.util.Stack;
  * @author pawar5658
  */
 public class Statemanager {
+    // Create instance variables
+
     private Stack<State> states;
-    
-    public Statemanager(){
+
+    /**
+     * Method to construct the state manager
+     */
+    public Statemanager() {
         states = new Stack<State>();
     }
-    
-    public void push(State s){
+
+    /**
+     * Method to move between states (pushing)
+     * @param s
+     */
+    public void push(State s) {
         states.push(s);
     }
-    
-    public void pop(){
+
+    /**
+     * Method to move between states (popping)
+     */
+    public void pop() {
         State s = states.pop();
         s.dispose();
     }
-    
-    public void set(State s){
+
+    /**
+     * Method to set states
+     * @param s
+     */
+    public void set(State s) {
         pop();
         push(s);
     }
-    
-    public void update(float deltaTime){
+
+    /**
+     * Method that updates the states
+     * @param deltaTime
+     */
+    public void update(float deltaTime) {
         states.peek().update(deltaTime);
     }
-    
-    public void render(SpriteBatch batch){
+
+    /**
+     * Method that renders the states
+     * @param batch
+     */
+    public void render(SpriteBatch batch) {
         states.peek().render(batch);
     }
 
+    /**
+     * Method that handles the input of the states
+     */
     public void handleInput() {
         states.peek().handleInput();
     }
-    
 }
