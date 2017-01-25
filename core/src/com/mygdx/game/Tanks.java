fa1ab9c67bebe17b9d20d5e74acbadbd27432355
dhila4674 <dhila4674@HRH-IC0027341.wrdsb.ca>
2017-01-25 12:21:31 -0500

    bullet2 done

--- core/src/com/mygdx/game/Tanks.java
+++ core/src/com/mygdx/game/Tanks.java
@@ -22,52 +22,50 @@ public class Tanks {
     private Vector3 position;
 
     public Tanks(int x, int y) {
+        // sets the tank into the game
         position = new Vector3(x,y,0); 
-        // 
         tankr = new Texture ("Tank6.png"); 
-        // 
         bounds = new Rectangle(position.x,position.y,tankr.getWidth(),tankr.getHeight());
         position.x = x;
         position.y = y;
     }
     
-    //
+    // Draws the red tank
     public void render(SpriteBatch batch) {
         batch.draw(tankr, position.x, position.y);
     }
     
-    //
+  
     public void update(float deltaTime) {
         // Updating the certain bounds of tankX
         bounds.setPosition(position.x, position.y);
     }
     
-    //
+    
     public float getX() {
         return position.x;
     }
-    
-    //
+
     public float getY() {
         return position.y;
     }
     
-    //
+    // Updates the hit box of the tank
     public Rectangle getBounds() {
         return bounds;
     }
     
-    //
+    //this runs the speed of the tank with the x position to move right 
     public float Accelerate(){
         return position.x = position.x + Accelerate;
     }
     
-    // 
+    // this runs the speed of the tank with the x position to move left
     public float Deccelerate(){
         return position.x = position.x - deccelerate;
     }
    
-    //
+    //this gets rid of tank when the game ends
     public void dispose() {
         tankr.dispose();
     }
