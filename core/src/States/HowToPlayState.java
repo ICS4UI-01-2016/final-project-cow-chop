f39d3b8a2c3470b7b8e48b4f6000644b4d3268bf
kulla6503 <kulla6503@HRH-IC0037385.wrdsb.ca>
2017-01-25 09:57:58 -0500

    Got Textures working with menu state

---
+++ core/src/States/HowToPlayState.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package States;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ *
+ * @author kulla6503
+ */
+class HowToPlayState extends State {
+    
+    public HowToPlayState(Statemanager gsm) {
+        super(gsm); 
+    }
+
+    @Override
+    public void render(SpriteBatch batch) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update(float deltaTime) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void handleInput() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void dispose() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
